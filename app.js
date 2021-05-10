939955eb83e9e721e82bc283dfcdc852d017fca7
John Carlo Cunanan <codingjohn@outlook.com>
2021-05-10 12:39:58 +0800

    Add morgan, express body parser, and middleware for unknown endpoint

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import morgan from "morgan";
 import logger from "./utils/logger.js";
 import config from "./utils/config.js";
+import middleware from "./utils/middleware.js";
 
 const app = express();
 
@@ -23,6 +25,11 @@ mongoose
     logger.info("Connection Error", error);
   });
 
+app.use(morgan("dev"));
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(middleware.unknownEndpoint);
 
 export default app;
